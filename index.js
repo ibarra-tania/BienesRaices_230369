20c8a920c777a10f5007bb51d4650a9d60809e60
Tania Ibarra Salgado. <230369@utxicotepec.edu.mx>
2024-10-21 12:09:15 -0600

    Práctica 09
    Se configuro el node, el json, realizamos un objeto y abrimos el host

---
+++ index.js
@@ -0,0 +1,25 @@
+const express= require(`express`); //declaración de objeto que permite entrar al protocolo http y leer páginas
+//importar la libreria para crear un servidos web
+
+//Instanciar nuestra aplicación web
+const app=express()
+
+//configuramos nuestro servidor web
+const port= 3000; 
+app.listen(port, ()=>{
+    console.log(`La aplicación ha iniciado en el puerto: ${port}`);
+})
+
+// Probamos las rutas para poder presentar mensajes al usuario a traves del navegador
+app.get("/", function(req, res){
+    res.send("Hola Mundo desde Node, a través del Navegador");
+})
+
+app.get("/QuienSoy", function(req, res){
+    res.json({"estudiante": "Tania Ibarra Salgado",
+        "carrera": "TI DSM",
+        "grado": "4°",
+        "grupo": "B",
+        "asignatura": "AWOS"
+    })
+})
